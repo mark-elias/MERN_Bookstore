37de424b4878b355770aaef77ee200512b816871
mark-elias <mark7elias@gmail.com>
2024-09-09 23:54:57 -0700

    working Post endpoint for adding books

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,38 @@
 import express from "express";
 import { PORT, MONGO_DB_URL } from "./config";
 import mongoose from "mongoose";
+import { BookModel, IBook } from "./models/book";
 
 const app = express();
+app.use(express.json());
 //===========================
 app.get("/", (req, res) => {
   console.log(req);
   res.send("hello world");
+});
+
+app.post("/books", (req, res) => {
+  // make a new object
+  // get req.body values for object
+  const newBook: IBook = {
+    title: req.body.title,
+    author: req.body.author,
+    publishYear: req.body.publishYear,
+  };
+
+  // make a new instance of BookModel
+  // using the object you created
+  const book = new BookModel(newBook);
+
+  //save the book object/instance to the Database
+  book
+    .save()
+    .then((savedBook) => res.status(201).send(savedBook))
+    .catch((err) => {
+      // Handle errors, such as validation errors
+      res.status(400).json({ message: err.message });
+      console.log("holyyyy");
+    });
 });
 
 //=============================
