77be8762cc21b2c0ce4c5ebbda1a08967d5e0fd8
mark-elias <mark7elias@gmail.com>
2024-09-19 22:25:25 -0700

    build

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -10,7 +10,9 @@ const booksRoute_1 = __importDefault(require("./routes/booksRoute"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: "https://mernbookstorefrontend.vercel.app/",
+}));
 app.use("/books", booksRoute_1.default);
 app.get("/", (req, res) => {
     res.send("hello world");
